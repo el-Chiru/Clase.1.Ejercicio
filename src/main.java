4f17976cdce2150834fc73cdb086dd3638f3238f
Gabi <125154797+el-Chiru@users.noreply.github.com>
2023-11-06 13:51:10 -0300

    Add files via upload

--- src/main.java
+++ src/main.java
@@ -1,30 +1,28 @@
 import ValidadorCorrelativas.Alumno;
 import ValidadorCorrelativas.Inscripcion;
 import ValidadorCorrelativas.Materia;
-import junit.framework.Assert;
-import junit.framework.AssertionFailedError;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class main {
-
     Materia estructuraDeDatos;
     Materia paradigmasDeProgramacion;
     Materia disenoDeSistemas;
-
-     @Test
-   public void GabrielSePuedeAnotarAAlgoritmos(){
+    Materia algoritmos;
+       @Test
+   public void GabrielSePuedenAnotarAAlgoritmos(){
         Alumno gabriel = new Alumno("Gabriel", "261915");
-        Materia algoritmos = new Materia("Algoritmos");
         Inscripcion inscripcionDeGabriel = new Inscripcion(gabriel, algoritmos);
-
-         algoritmos.agregarCorrelativa(estructuraDeDatos);
-         paradigmasDeProgramacion.agregarCorrelativa(algoritmos);
-         disenoDeSistemas.agregarCorrelativa(paradigmasDeProgramacion);
-
-         Inscripcion gabrielSePuedeAnotarAAlgoritmos  = new Inscripcion(gabriel, algoritmos);
-
-}
+   }
+   @Test
+    public void SofiaSePuedenAnotarAEstructuraDeDatos() {
+        Alumno sofia = new Alumno("Sofia", "224563");
+        estructuraDeDatos.agregarCorrelativa(algoritmos);
+        Inscripcion inscripcionDeSofia = new Inscripcion(sofia, estructuraDeDatos);
+    }
 }
 
